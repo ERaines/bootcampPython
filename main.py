4ed7524ba2bfbe8d8846880eed71b9270feae889
ERaines <dash591@gmail.com>
2025-07-28 20:00:35 +0100

    If else

--- main.py
+++ main.py
@@ -8,6 +8,19 @@
 # print(f'Olá {name}, vote tem {idade} e sua fruta favorita é {fruta_favorita}')
 
 
-produto = int('Quantos gramas tem o Produto')
-uso_diario = int('Quantas gramas porçoes por dia você precisa usar?')
-porcao = int(45)
+# produto_porcoes = int(input('Quantas porçoes o produto tem?'))
+# uso_diario = int(input('Quantas porçoes vc usar por dia?'))
+
+# dias = produto_porcoes / uso_diario
+# print(f') produtudo vai durar {dias:.0f} dias')
+# print(f') produtudo vai durar {int(dias)} dias')
+
+
+nota_aluno = float(input('Qual foi sua nota no exame'))
+
+if nota_aluno >= 7:
+    print('Você esta aprovado!')
+elif nota_aluno >= 5:
+    print('Você esta de recuperação')
+else:
+    print('Você esta reprovado!')
