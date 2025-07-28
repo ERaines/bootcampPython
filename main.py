ff6349d91eefd4c3ec98c6e840722b552f9b45c1
ERaines <dash591@gmail.com>
2025-07-28 11:28:37 +0100

    creating a daily usage calculation for the product

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
-pname = input('Seu nome')
-pidade = input('Idade')
-print(pname)
-print(pidade)
+
+# F - String
+# name = input('Qual é o seu nome?')
+# idade = input('Qual é sua idade?')
+# fruta_favorita = input('Qual é sua fruta favorita')
+
+# print(f'Olá {name}, vote tem {idade} e sua fruta favorita é {fruta_favorita}')
+
+
+produto = int('Quantos gramas tem o Produto')
+uso_diario = int('Quantas gramas porçoes por dia você precisa usar?')
+porcao = int(45)
